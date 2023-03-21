7c81ed3d8cb1d201120e22ebae297e0713912b9c
juanBailon <juanojo2001.5@gmail.com>
2023-03-21 18:48:01 -0500

    adding apps to the installed_apps in  settings.py

--- ApiweedWeb/backend_weed_web/backend_weed_web/settings.py
+++ ApiweedWeb/backend_weed_web/backend_weed_web/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
+    'seeds',
+    'seed_properties',
+    'farms',
+    
 ]
 
 MIDDLEWARE = [
