b092413af9f49fa3d0674fda48abd47800e50a3a
Alexander <jhon.hilamo@correounivalle.edu.co>
2023-03-22 00:28:36 -0500

    changes user's models

--- ApiweedWeb/backend_weed_web/users/models.py
+++ ApiweedWeb/backend_weed_web/users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-<<<<<<< HEAD
 #User's model with the atributes: id_user, password, user_email and phone_number
 #act: 21/03/2023
 id_type=(('C.C','C.C'),('NIT','NIT'))
@@ -18,9 +17,4 @@ class User(models.Model):
     user_email=models.EmailField(null = False, blank = False, max_length=254)
     phone_number=models.CharField(null = False, blank = False, max_length=12)
     
-=======
 # Create your models here.
-
-class User(models.Model):
-    pass
->>>>>>> e4d2a5ff5de7e7a53169c1d396f2abf44fa82ae2
