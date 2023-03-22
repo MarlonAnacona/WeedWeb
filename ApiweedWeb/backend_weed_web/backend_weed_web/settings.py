72f4dba98bbbaec4cb89c42d05c464e36d25691a
juanBailon <juanojo2001.5@gmail.com>
2023-03-21 22:45:07 -0500

    setting up DB conection to postgres, adding  basic(bare bones) user model to app users so that the farm model doesnt show an error on the FK

--- ApiweedWeb/backend_weed_web/backend_weed_web/settings.py
+++ ApiweedWeb/backend_weed_web/backend_weed_web/settings.py
@@ -10,9 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 
 from datetime import timedelta
 from pathlib import Path
+
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,8 +151,12 @@ WSGI_APPLICATION = 'backend_weed_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
