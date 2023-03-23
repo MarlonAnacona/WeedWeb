9dc786150d79eba6e2cf18e13bcac54141223b8b
Lau36 <laura.jaimes@correounivalle.edu.co>
2023-03-22 19:19:58 -0500

    Interface userRegister

--- weedWeb/src/app/model/interfaces.ts
+++ weedWeb/src/app/model/interfaces.ts
@@ -1,23 +1,30 @@
-export interface user {
-  nombre: string;
-  apellido: string;
+export interface userRegister {
+  firstName: string;
+  lastName: string;
   email: string;
-  edad: number;
-  nit: string;
+  nationalId: number;
+  phoneNumber:number;
   password: string;
 }
 
-export interface company {
-  nombre: string;
-  apellido: string;
+export interface user {
+  firstName: string;
+  lastName: string;
   email: string;
-  edad: number;
-  nit: string;
+  nationalId: number;
+  phoneNumber:number;
   password: string;
+}
+
+export interface companyRegister {
+  name: string;
+  email: string;
+  password: string;
+  nit: number;
+  phoneNumber:number;
 }
 
 export interface userLogin {
   email: string;
-
   password: string;
 }
