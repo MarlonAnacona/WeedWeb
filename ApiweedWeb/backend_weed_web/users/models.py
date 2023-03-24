65fba422ec4d69892a1f932c763a76c296ce8d8b
juanBailon <juanojo2001.5@gmail.com>
2023-03-24 01:02:11 -0500

    ajustes modelo customer y view para login realizado

--- ApiweedWeb/backend_weed_web/users/models.py
+++ ApiweedWeb/backend_weed_web/users/models.py
@@ -1,15 +1,17 @@
 from django.core import validators
 from django.db import models
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
 
 class CustomerManager(BaseUserManager):
-    def create_user(self, email, password, first_name, last_name, phone_number, **extra_fields):
+    def create_user(self, email, password, phone_number, **extra_fields):
         # Create and save a new user with the given email and password
-        user = self.model(email=email, first_name=first_name, last_name=last_name, phone_number=phone_number, **extra_fields)
+        user = self.model(email=self.normalize_email(email), phone_number=phone_number, **extra_fields)
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
+        
         return user
 
     def create_superuser(self, email, password, **extra_fields):
@@ -28,30 +30,52 @@ class CustomerManager(BaseUserManager):
                                 phone_number=phone,
                                 **extra_fields
                                 )
-        # user.is_staff = True
-        # user.is_superuser = True
         user.save()
         return user
 
 
 class Customer(AbstractUser, PermissionsMixin):
     
-    email = models.EmailField(unique=True)
+    username = None
+    first_name = None
+    last_name = None
+    email = models.EmailField(unique=True) 
     password = models.CharField(max_length=200, validators=[validators.MinLengthValidator(8)])
-    first_name = models.CharField(max_length=30, blank=False, null=False)
-    middle_name = models.CharField(max_length=30, blank=True, null=True)
-    last_name = models.CharField(max_length=30, blank=False, null=False)
     phone_number = models.CharField(max_length=10, validators=[validators.MinLengthValidator(10)], blank=False, null=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     
     
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number']
+    REQUIRED_FIELDS = ['phone_number']
 
     objects = CustomerManager()
 
     def __str__(self):
-        return f'customer: {self.first_name} \n  email: {self.email}'
+        return f'customer email: {self.email}'
 
 
+
+class Person(Customer):
+
+    national_id = models.CharField(max_length=20,
+                                   validators=[validators.RegexValidator(r'^\d{1,20}$', 'Enter a valid national ID (up to 20 digits)')],
+                                   blank=False,
+                                   null=False
+                                   )
+    first_name = models.CharField(max_length=30, blank=False, null=False)
+    middle_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=False, null=False)
+
+
+    def __str__(self):
+        return f'customer(person): {self.first_name} {self.last_name} \n   email: {self.email}'
+
+
+class Company(Customer):
+    
+    NIT = models.CharField(max_length=30, blank=False, null=False)
+    name = models.CharField(max_length=50, blank=False, null=False)
+    
+    def __str__(self):
+        return f'customer(company): {self.name}\n   email: {self.email}'
--- ApiweedWeb/backend_weed_web/users/urls.py
+++ ApiweedWeb/backend_weed_web/users/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name='users'
 
-urlpatterns = [     
+urlpatterns = [ 
+    path('api/token/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', views.TokenRefreshView.as_view(), name='token_refresh'),    
     
 ]
