73687b60d22126bf6e89cf510b46cc5bb53d0b12
unknown <jesus.guetio@correounivalle.edu.co>
2023-03-28 18:58:15 -0500

    seed properties update

--- ApiweedWeb/backend_weed_web/seed_properties/models.py
+++ ApiweedWeb/backend_weed_web/seed_properties/models.py
@@ -1,38 +1,23 @@
 from django.db import models
-from seed.models import Seeds
+from seeds.models import Seeds
 
 # Create your models here.
 
-class SeedFlavor(models.Model):
+class SeedProperties(models.Model): 
     seed_id = models.ForeignKey(Seeds, on_delete=models.CASCADE, null=False)
-    flavor_id = models.CharField(max_length=50)
-
-class Flavor(models.Model):
-    id = models.ForeignKey(SeedFlavor, on_delete=models.CASCADE, null =False)
-    flavor_name= models.CharField(max_length=50)
+    flavor_name = models.CharField(max_length=50)
+    effect_name = models.CharField(max_length=50)
+    smell_name = models.CharField(max_length=50)
 
     def __str__(self):
+        return f'{self.flavor_name}, {self.effect_name}, {self.smell_name}'
+    
+    def get_flavor(self):
         return self.flavor_name
-
-class SeedSmell(models.Model):
-    seed_id =models.ForeignKey(Seeds, on_delete=models.CASCADE, null=False)
-    smell_id= models.CharField(max_length=50)
-
-class Smell(models.Model):
-    id = models.ForeignKey(SeedSmell, on_delete= models.CASCADE, null =False)
-    smell_name= models.CharField(max_length=50)
-
-    def __str__(self):
+    
+    def get_effect(self):
+        return self.effect_name
+    
+    def get_smell(self):
         return self.smell_name
 
-class SeedEffect(models.Model):
-    seed_id= models.ForeignKey(Seeds, on_delete=models.CASCADE, null=False)
-    effect_id= models.CharField(max_length=50)
-
-
-class Effect(models.Model):
-    id = models.ForeignKey(SeedEffect, on_delete= models.CASCADE, null=False)
-    effect_name= models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.effect_name
