44a1fe6a5a45ba23c41061b19f71aa69930c2f98
unknown <jesus.guetio@correounivalle.edu.co>
2023-03-28 19:06:05 -0500

    properties model

--- ApiweedWeb/backend_weed_web/seed_properties/models.py
+++ ApiweedWeb/backend_weed_web/seed_properties/models.py
@@ -9,15 +9,19 @@ class SeedProperties(models.Model):
     effect_name = models.CharField(max_length=50)
     smell_name = models.CharField(max_length=50)
 
+    #get all properties
     def __str__(self):
         return f'{self.flavor_name}, {self.effect_name}, {self.smell_name}'
     
+
     def get_flavor(self):
         return self.flavor_name
     
+
     def get_effect(self):
         return self.effect_name
     
     def get_smell(self):
         return self.smell_name
+    
 
